155d87be44c0c54a12049197266bbbcdfb4f992f
Andy LaBounty <alabounty23@gmail.com>
2021-08-10 22:07:51 -0500

    County analysis for challege #3

--- PyPoll.py
+++ PyPoll.py
@@ -5,6 +5,7 @@ import csv
 import os
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+print(file_to_load)
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
